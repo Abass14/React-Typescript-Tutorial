ec2cae19970e66140f05c52f1ee123b697e04ab2
Abass14 <abasstoy14@gmail.com>
2021-10-19 02:56:01 +0100

    completed tutorial

---
+++ src/component/Container.tsx
@@ -0,0 +1,12 @@
+import { type } from "os"
+
+type ContainerProps = {
+    styles: React.CSSProperties
+}
+export const Container = (props: ContainerProps) =>{
+    return(
+        <div style={props.styles}>
+            Text content goes here
+        </div>
+    )
+}
