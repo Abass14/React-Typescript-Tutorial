ad6bda7a34264c891bc9365bb2aa4779c1190b95
Abass14 <abasstoy14@gmail.com>
2021-10-19 19:26:42 +0100

    completed tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { Oscar } from './component/Oscar';
 import { Person } from './component/Person';
 import { PersonList } from './component/PersonList';
 import { LoggedIn } from './component/state/LoggedIn';
+import { User } from './component/state/User';
 import { Status } from './component/Status';
 
 function App() {
@@ -55,6 +56,7 @@ function App() {
       <Button handleClick={handleClicked} />
       <Container styles={{ border: '1px solid black', padding: '2rem'}} />
       <LoggedIn />
+      <User />
     </div>
   );
 }
